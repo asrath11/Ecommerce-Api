2232e41f763d59caa217fcf25247985cda4007d9
Asrath <pasrath.2004@gmail.com>
2024-12-04 21:18:27 +0530

    removed swagger

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const morgan = require('morgan');
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
 
 const userRouter = require('./routes/userRoutes');
 const productRouter = require('./routes/productRoutes');
@@ -14,31 +12,6 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-
-// Swagger Documentation Setup
-const swaggerOptions = {
-  definition: {
-    openapi: '3.0.0', // Specify OpenAPI version
-    info: {
-      title: 'Ecommerce API',
-      version: '1.0.0',
-      description: 'API documentation for the E-commerce platform.',
-      contact: {
-        name: 'Asrath',
-        email: 'pasrath.2004@gmail.com',
-      },
-    },
-    servers: [
-      {
-        url: 'http://localhost:3000', // Server URL
-      },
-    ],
-  },
-  apis: ['./routes/*.js'], // Points to the route files for documentation
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // API Routes
 app.use('/ecommerce/api/v1/users', userRouter);
